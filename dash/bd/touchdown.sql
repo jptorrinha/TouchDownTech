527f3373300e497bd86637dac4f9cbb5cde69630
Joao Paulo Gomes de Oliveira <joao@MacBook-Pro-de-Joao.local>
2024-12-16 10:49:14 -0300

    Ajuste do BD

---
+++ dash/bd/touchdown.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Tempo de geração: 16/12/2024 às 13:48
+-- Versão do servidor: 8.0.40
+-- Versão do PHP: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `touchdown`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `BILLINGS`
+--
+
+CREATE TABLE `BILLINGS` (
+  `bill_id` varchar(64) NOT NULL,
+  `bill_player_id` varchar(64) NOT NULL,
+  `bill_reference` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `TEAMS`
+--
+
+CREATE TABLE `TEAMS` (
+  `team_id` varchar(64) NOT NULL,
+  `team_nome` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `USUARIOS`
+--
+
+CREATE TABLE `USUARIOS` (
+  `U_id` varchar(36) NOT NULL,
+  `U_nome` varchar(255) NOT NULL,
+  `U_email` varchar(255) NOT NULL,
+  `U_telefone` varchar(30) NOT NULL,
+  `u_perfil` set('1','2','3') CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '1 - Admin, 2 - Treinador, 3 - Atleta',
+  `U_senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `BILLINGS`
+--
+ALTER TABLE `BILLINGS`
+  ADD PRIMARY KEY (`bill_id`);
+
+--
+-- Índices de tabela `TEAMS`
+--
+ALTER TABLE `TEAMS`
+  ADD PRIMARY KEY (`team_id`);
+
+--
+-- Índices de tabela `USUARIOS`
+--
+ALTER TABLE `USUARIOS`
+  ADD PRIMARY KEY (`U_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
