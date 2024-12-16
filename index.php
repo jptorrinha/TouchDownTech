2cc5ed34454dad6082b408af4d7c97272d435cc5
Joao Paulo Gomes de Oliveira <joao@MacBook-Pro-de-Joao.local>
2024-12-16 10:07:42 -0300

    Ajuste de P'sginas

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include 'parts/header.php' ?>
 <main>
-    <div class="container-fluid admin">
+    <div class="container-fluid admin" style="display: none">
         <div class="container">
             <div class="row mb-4">
                 <div class="col-12">
@@ -37,10 +37,67 @@
         </div>
     </div>
 
-    <div class="container-fluid couchs">
+    <div class="container-fluid couchs" style="display: none">
         <div class="container">
-            <div class="row">
-                <div class="col-12">Treinadores</div>
+            <div class="row mb-4">
+                <div class="col-12">
+                    <p class="text-center">O que você que fazer hoje?</p>
+                </div>
+            </div>
+            <div class="row mb-5">
+                <div class="col-12">
+                    <div class="d-grid gap-2 box-buttons">
+                        <a href="upload.php" class="btn btn-outline-info btn-lg lk">
+                            <i class="bi bi-cloud-arrow-up"></i><br>Upload de vídeos
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-5">
+                <div class="col-12">
+                    <div class="d-grid gap-2 box-buttons">
+                        <a href="analise.php" class="btn btn-outline-info btn-lg lk">
+                            <i class="bi bi-graph-up-arrow"></i><br>Acessar minhas análises
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-5">
+                <div class="col-12">
+                    <div class="d-grid gap-2 box-buttons">
+                        <a href="gerencia.php" class="btn btn-outline-info btn-lg lk">
+                            <i class="bi bi-gear"></i><br>Gerenciar meu time
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid player">
+        <div class="container">
+            <div class="row mb-4">
+                <div class="col-12">
+                    <p class="text-center">O que você que fazer hoje?</p>
+                </div>
+            </div>
+            <div class="row mb-5">
+                <div class="col-12">
+                    <div class="d-grid gap-2 box-buttons">
+                        <a href="analise.php" class="btn btn-outline-info btn-lg lk">
+                            <i class="bi bi-graph-up-arrow"></i><br>Acessar minhas análises
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-5">
+                <div class="col-12">
+                    <div class="d-grid gap-2 box-buttons">
+                        <a href="gerencia.php" class="btn btn-outline-info btn-lg lk">
+                            <i class="bi bi-cash-coin"></i><br>Registrar pagamento de mensalidade
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
