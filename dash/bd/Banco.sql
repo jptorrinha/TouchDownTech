d8c13d1f84ec4622a8fdc17cd020707ea92158ad
Joao Paulo Gomes de Oliveira <joao@MacBook-Pro-de-Joao.local>
2024-12-17 09:16:56 -0300

    Ajustes do paymment"

--- dash/bd/Banco.sql
+++ dash/bd/Banco.sql
@@ -1,53 +1,16 @@
-
-CREATE TABLE `DIRECTOR` (
-  `director_id` varchar(64) NOT NULL,
-  `director_nome` varchar(255) NOT NULL,
-  `director_email` varchar(255) NOT NULL,
-  `director_senha` varchar(255) NOT NULL
+CREATE TABLE `USUARIOS` (
+  `U_id` varchar(36) NOT NULL,
+  `U_nome` varchar(255) NOT NULL,
+  `U_email` varchar(255) NOT NULL,
+  `U_telefone` varchar(30) NOT NULL,
+  `U_senha` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
---
--- Indexes for table `DIRECTOR`
---
-ALTER TABLE `DIRECTOR`
-  ADD PRIMARY KEY (`director_id`,`director_email`);
---
--- AUTO_INCREMENT for table `DIRECTOR`
---
-
-CREATE TABLE `COUCHS` (
-  `couch_id` varchar(64) NOT NULL,
-  `couch_nome` varchar(255) NOT NULL,
-  `couch_email` varchar(255) NOT NULL,
-  `couch_team` varchar(255) NOT NULL,
-  `couch_senha` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
---
--- Indexes for table `COUCHS`
---
-ALTER TABLE `COUCHS`
-  ADD PRIMARY KEY (`couch_id`,`couch_email`);
---
--- AUTO_INCREMENT for table `COUCHS`
---
-
-CREATE TABLE `PLAYERS` (
-  `player_id` varchar(64) NOT NULL,
-  `player_nome` varchar(255) NOT NULL,
-  `player_email` varchar(255) NOT NULL,
-  `player_team` varchar(255) NOT NULL,
-  `player_senha` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 --
 -- Indexes for table `PLAYERS`
 --
-ALTER TABLE `PLAYERS`
-  ADD PRIMARY KEY (`player_id`,`player_email`);
+ALTER TABLE `USUARIOS`
+  ADD PRIMARY KEY (`U_id`);
 --
 -- AUTO_INCREMENT for table `PLAYERS`
 --
@@ -60,12 +23,12 @@ CREATE TABLE `TEAMS` (
 
 
 --
--- Indexes for table `PLAYERS`
+-- Indexes for table `TEAMS`
 --
-ALTER TABLE `PLAYERS`
+ALTER TABLE `TEAMS`
   ADD PRIMARY KEY (`team_id`);
 --
--- AUTO_INCREMENT for table `PLAYERS`
+-- AUTO_INCREMENT for table `TEAMS`
 --
 
 CREATE TABLE `BILLINGS` (
@@ -76,10 +39,10 @@ CREATE TABLE `BILLINGS` (
 
 
 --
--- Indexes for table `PLAYERS`
+-- Indexes for table `BILLINGS`
 --
-ALTER TABLE `PLAYERS`
+ALTER TABLE `BILLINGS`
   ADD PRIMARY KEY (`bill_id`);
 --
--- AUTO_INCREMENT for table `PLAYERS`
+-- AUTO_INCREMENT for table `BILLINGS`
 --
